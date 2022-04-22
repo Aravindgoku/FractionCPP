184ab66d576a093e83e54699ad593c50d4b5f9bf
Aravindgoku <60890966+Aravindgoku@users.noreply.github.com>
2022-04-22 01:28:28 -0400

    Update Fraction.h
    Added changes for the Fraction class

--- Mathworks/Fraction/Fraction.h
+++ Mathworks/Fraction/Fraction.h
@@ -1,34 +1,41 @@
+/**
+ * @class Fraction
+ * @author Aravind S
+ * @date 04/21/2022
+ * @file Fraction.h
+ * @brief Fraction class that performs basic math operations between fraction objects.
+ * Throws Integer overflow and Divide by zero exception through std::overflow_error class
+ */
 #pragma once
+#include <iostream>
+#include <stdexcept>
 
 class Fraction
 {
+    
+    friend std::ostream& operator<<(std::ostream &os, const Fraction &rhs);
+    
 private:
     int numerator;
     int denominator;
 public:
     Fraction(int num, int den);
     Fraction();
-    //Fraction(const Fraction &source);
-    //template <typename T>
-    Fraction operator+(const Fraction &rhs);
-    Fraction operator-(const Fraction &rhs);
-    Fraction operator*(const Fraction &rhs);
-    Fraction operator/(const Fraction &rhs);
-    void operator=(const Fraction &rhs);
+    Fraction operator+(const Fraction &rhs) const;
+    Fraction operator-(const Fraction &rhs) const;
+    Fraction operator*(const Fraction &rhs) const;
+    Fraction operator/(const Fraction &rhs) const;
+    //void operator=(const Fraction &rhs);
     
     bool operator==(const Fraction &rhs) const;
     bool operator!=(const Fraction &rhs) const;
     bool operator>(const Fraction &rhs) const;
     bool operator<(const Fraction &rhs) const;
-    ~Fraction();
-    
-    void display() const;
-    
+        
     int getNumerator() const;
     int getDenominator() const;
-    
-    /*void setNumerator(int);
-    void setDenominator(int);*/
 
 };
 
+    void multiplicationCheck(int num1, int num2, int &result);
+    void additionCheck(int num1, int num2, int &result);
